9475050a9bcb8104be565b2f2abe359d8bef3a01
ovcovc <piotrolejnik88@gmail.com>
2015-03-19 14:52:09 +0100

    cookies introduced

--- chat_room/views.py
+++ chat_room/views.py
@@ -7,14 +7,28 @@ from chat_room.models import Message
 
 
 def chat_room(request):
+    name = ''
     if request.method == 'POST':
         form = ChatForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['name']
-            text = form.cleaned_data['text']
-            message = Message(name=name, text=text)
-            message.save()
+        if 'change' in request.POST:
+            try:
+                del request.session['name']
+            except KeyError:
+                pass
+        else:
+            if 'name' in request.session:
+                name = request.session['name']
+            if form.is_valid():
+                name = form.cleaned_data['name']
+                request.session['name'] = name
+                text = form.cleaned_data['text']
+                message = Message(name=name, text=text)
+                message.save()
     else:
-        form = ChatForm()
-    messages = Message.objects.all().order_by('-created_at')
-    return render(request, 'chat.html', {'form': form, 'messages':messages})
+        if 'name' in request.session:
+            name = request.session['name']
+            form = ChatForm(initial={'name': name})
+        else:
+            form = ChatForm()
+    messages = Message.objects.all().order_by('created_at')
+    return render(request, 'chat.html', {'form': form, 'messages':messages, 'name': name})
